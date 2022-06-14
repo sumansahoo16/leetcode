1541a7e44ad8aec424b85eb27b6aca2ac3d4d05d
Suman Sahoo <suman5sahoo@gmail.com>
2022-06-14 09:14:40 +0530

    Queries on Number of Points Inside a Circle
    improved speed and sp[ace

--- 1828.py
+++ 1828.py
@@ -1,14 +1,12 @@
 class Solution:
     def countPoints(self, points: List[List[int]], queries: List[List[int]]) -> List[int]:
         
-        def is_inside(q, p):
-            distance = (q[0] - p[0]) ** 2 + (q[1] - p[1]) ** 2
-            if distance <= q[2] * q[2] : return 1
-            return 0
-        ans = []
-        for q in queries:
+        ans = [0] * len(queries)
+        for i, q in enumerate(queries):
             count = 0
             for p in points:
-                count += is_inside(q, p) 
-            ans.append(count)
+                if  (q[0] - p[0]) ** 2 + (q[1] - p[1]) ** 2 <= q[2] * q[2] : count += 1
+            ans[i] = count
         return ans 
+            
+        
