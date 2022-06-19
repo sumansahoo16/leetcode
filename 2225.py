00573847707f48e5fc26a49321359c47bf9815d8
Suman Sahoo <suman5sahoo@gmail.com>
2022-06-19 15:27:18 +0530

    Find Players With Zero or One Losses
    Less space

--- 2225.py
+++ 2225.py
@@ -1,26 +1,14 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         
-        players = {}
+        lost = Counter()
+        winners = set()
         
-        for m in matches:
-            
-            if m[0] not in players.keys():
-                players[m[0]] = [0, 0]
-            if m[1] not in players.keys():
-                players[m[1]] = [0, 0]
-                
-            players[m[0]][0] += 1 
-            players[m[1]][1] += 1 
-            
+        for w, l in matches:
+            winners.add(w)
+            lost[l] += 1
         
-        won = []
-        lost = []
+        one = {k for k, v in lost.items() if v == 1}
+        zero = winners - set(lost.keys())
         
-        for p in sorted(players.keys()):
-            
-            
-            if players[p][1] == 0 : won.append(p)
-            elif players[p][1] == 1 : lost.append(p)
-                
-        return [won, lost]
+        return [sorted(zero), sorted(one)]
