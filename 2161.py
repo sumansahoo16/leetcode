876565504916c71130563e71f2729fbfccd4682e
Suman Sahoo <suman5sahoo@gmail.com>
2022-06-26 00:01:46 +0530

    Partition Array According to Given Pivot

--- 2161.py
+++ 2161.py
@@ -1,6 +1,6 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        
+        """
         A = []
         P = 0
         Z = [] 
@@ -11,4 +11,34 @@ class Solution:
             else : P += 1
                 
         return A + [pivot] * P + Z
+        """
+        
+        SORTED = [0] * len(nums)
+        
+        a, p, z = 0, 0, 0
+        
+        for n in nums : 
+            if n > pivot : z += 1
+            elif n < pivot : a += 1
+            else : p += 1
+                
+        a_idx = 0
+        p_idx = a 
+        z_idx = a + p
+        
+        for n in nums:
+            if n > pivot :
+                SORTED[z_idx] = n
+                z_idx += 1
+                
+            elif n < pivot:
+                SORTED[a_idx] = n
+                a_idx += 1
+                
+            else :
+                SORTED[p_idx] = n
+                p_idx += 1
+                
+        return SORTED
+            
         
